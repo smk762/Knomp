c9ae86e81d94411f945c8420c7ce1a343fa53548
smk762 <35845239+smk762@users.noreply.github.com>
2018-09-14 16:48:09 +0800

    included skip condition

--- gencfg.sh
+++ gencfg.sh
@@ -34,24 +34,29 @@ for row in $(echo "${ac_json}" | jq -c -r '.[]'); do
      echo ${row} | jq -r ${1}
     }
 	chain=$(_jq '.ac_name')
-
-	string=$(printf '%08x\n' $(komodo-cli -ac_name=$chain getinfo | jq '.magic'))
-    magic=${string: -8}
+	
+#Get Asset Chain Names from json file
+  if [[ " ${skip[@]} " =~ " ${chain} " ]]; then
+   	pointless=0
+  else
+  	string=$(printf '%08x\n' $(komodo-cli -ac_name=$chain getinfo | jq '.magic'))
+    	magic=${string: -8}
 	magicrev=$(echo ${magic:6:2}${magic:4:2}${magic:2:2}${magic:0:2})
 
 	p2pport=$(komodo-cli -ac_name=$chain getinfo | jq '.p2pport')
-    thisconf=$(<~/.komodo/$chain/$chain.conf)
+   	thisconf=$(<~/.komodo/$chain/$chain.conf)
 
-    rpcuser=$(echo $thisconf | grep -Po "rpcuser=(\S*)" | sed 's/rpcuser=//')
-    rpcpass=$(echo $thisconf | grep -Po "rpcpassword=(\S*)" | sed 's/rpcpassword=//')
-	rpcport=$(echo $thisconf | grep -Po "rpcport=(\S*)" | sed 's/rpcport=//')
+   	rpcuser=$(echo $thisconf | grep -Po "rpcuser=(\S*)" | sed 's/rpcuser=//')
+   	rpcpass=$(echo $thisconf | grep -Po "rpcpassword=(\S*)" | sed 's/rpcpassword=//')
+  	rpcport=$(echo $thisconf | grep -Po "rpcport=(\S*)" | sed 's/rpcport=//')
 
 	echo "$cointemplate" | sed "s/COINNAMEVAR/$chain/" | sed "s/MAGICREVVAR/$magicrev/" > $coinsdir/$chain.json
-    echo "$pooltemplate" | sed "s/P2PPORTVAR/$p2pport/" | sed "s/COINNAMEVAR/$chain/" | sed "s/WALLETADDRVAR/$walletaddress/" | sed "s/STRATUMPORTVAR/$stratumport/" | sed "s/RPCPORTVAR/$rpcport/" | sed "s/RPCUSERVAR/$rpcuser/" | sed "s/RPCPASSVAR/$rpcpass/" > $poolconfigdir/$chain.json
+   	echo "$pooltemplate" | sed "s/P2PPORTVAR/$p2pport/" | sed "s/COINNAMEVAR/$chain/" | sed "s/WALLETADDRVAR/$walletaddress/" | sed "s/STRATUMPORTVAR/$stratumport/" | sed "s/RPCPORTVAR/$rpcport/" | sed "s/RPCUSERVAR/$rpcuser/" | sed "s/RPCPASSVAR/$rpcpass/" > $poolconfigdir/$chain.json
 
-    echo "sudo ufw allow $stratumport comment 'Stratum $chain'" >> $ufwenablefile
-    echo "sudo ufw delete allow $stratumport" >> $ufwdisablefile
+    	echo "sudo ufw allow $stratumport comment 'Stratum $chain'" >> $ufwenablefile
+    	echo "sudo ufw delete allow $stratumport" >> $ufwdisablefile
 	let "stratumport = $stratumport + 1"
+  fi
 done
 
 chmod +x $ufwenablefile
