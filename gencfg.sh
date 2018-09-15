55d91a387fd380468f1bace4fcf0255afb6274a9
smk762 <35845239+smk762@users.noreply.github.com>
2018-09-15 17:06:34 +0800

    Update gencfg.sh

--- gencfg.sh
+++ gencfg.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 # Put the address to mine to here, this is smk762's please change to yours.
 walletaddress=RUpV4Mov3Soj34iVNq3hPMCoUFYtB1Jzuf
@@ -30,34 +31,38 @@ rm $ufwdisablefile
 #Get Asset Chain Names from json file
 ac_json=$(curl https://raw.githubusercontent.com/blackjok3rtt/StakedNotary/master/assetchains.json 2>/dev/null)
 for row in $(echo "${ac_json}" | jq -c -r '.[]'); do
-	_jq() {
-     		echo ${row} | jq -r ${1}
-    	}
-	chain=$(_jq '.ac_name')
+   _jq() {
+     echo ${row} | jq -r ${1}
+    }
+        chain=$(_jq '.ac_name')
 
 #Get Asset Chain Names from json file
   if [[ " ${skip[@]} " =~ " ${chain} " ]]; then
-   	pointless=0
+        pointless=0
   else
-  	string=$(printf '%08x\n' $(komodo-cli -ac_name=$chain getinfo | jq '.magic'))
-    	magic=${string: -8}
-	magicrev=$(echo ${magic:6:2}${magic:4:2}${magic:2:2}${magic:0:2})
+        string=$(printf '%08x\n' $(komodo-cli -ac_name=$chain getinfo | jq '.magic'))
+        magic=${string: -8}
+        magicrev=$(echo ${magic:6:2}${magic:4:2}${magic:2:2}${magic:0:2})
 
-	p2pport=$(komodo-cli -ac_name=$chain getinfo | jq '.p2pport')
-   	thisconf=$(<~/.komodo/$chain/$chain.conf)
+        p2pport=$(komodo-cli -ac_name=$chain getinfo | jq '.p2pport')
+        thisconf=$(<~/.komodo/$chain/$chain.conf)
 
-   	rpcuser=$(echo $thisconf | grep -Po "rpcuser=(\S*)" | sed 's/rpcuser=//')
-   	rpcpass=$(echo $thisconf | grep -Po "rpcpassword=(\S*)" | sed 's/rpcpassword=//')
-  	rpcport=$(echo $thisconf | grep -Po "rpcport=(\S*)" | sed 's/rpcport=//')
+        rpcuser=$(echo $thisconf | grep -Po "rpcuser=(\S*)" | sed 's/rpcuser=//')
+        rpcpass=$(echo $thisconf | grep -Po "rpcpassword=(\S*)" | sed 's/rpcpassword=//')
+        rpcport=$(echo $thisconf | grep -Po "rpcport=(\S*)" | sed 's/rpcport=//')
 
-	echo "$cointemplate" | sed "s/COINNAMEVAR/$chain/" | sed "s/MAGICREVVAR/$magicrev/" > $coinsdir/$chain.json
-   	echo "$pooltemplate" | sed "s/P2PPORTVAR/$p2pport/" | sed "s/COINNAMEVAR/$chain/" | sed "s/WALLETADDRVAR/$walletaddress/" | sed "s/STRATUMPORTVAR/$stratumport/" | sed "s/RPCPORTVAR/$rpcport/" | sed "s/RPCUSERVAR/$rpcuser/" | sed "s/RPCPASSVAR/$rpcpass/" > $poolconfigdir/$chain.json
+        echo "$cointemplate" | sed "s/COINNAMEVAR/$chain/" | sed "s/MAGICREVVAR/$magicrev/" > $coinsdir/$chain.json
+        echo "$pooltemplate" | sed "s/P2PPORTVAR/$p2pport/" | sed "s/COINNAMEVAR/$chain/" | sed "s/WALLETADDRVAR/$walletaddress/" | sed "s/STRATUMPORTVAR/$stratumport/" | sed "s/RPCPORTVAR/$rpcport/" | sed$
 
-    	echo "sudo ufw allow $stratumport comment 'Stratum $chain'" >> $ufwenablefile
-    	echo "sudo ufw delete allow $stratumport" >> $ufwdisablefile
-	let "stratumport = $stratumport + 1"
+        echo "sudo ufw allow $stratumport comment 'Stratum $chain'" >> $ufwenablefile
+        echo "sudo ufw delete allow $stratumport" >> $ufwdisablefile
+        let "stratumport = $stratumport + 1"
   fi
 done
 
 chmod +x $ufwenablefile
 chmod +x $ufwdisablefile
+
+
+
+
