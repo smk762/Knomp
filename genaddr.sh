f94db5a1a6f5e91c3b7a53a19cfa4c4988049f51
smk762 <smk762@iinet.net.au>
2018-10-02 14:11:59 +0000

    start and stop

---
+++ genaddr.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Get Asset Chain Names from json file
+echo -e "\e[91m WARNING: This script creates addresses to be use in pool config and payment processing"
+echo " The address, privkey, and pubkey are stored in a owner read-only file"
+echo -e " make sure to encrypt, backup, or delete as required \e[39m"
+if [ ! -d ~/kmd_pulp/stomp/wallets  ]; then
+	mkdir ~/kmd_pulp/stomp/wallets
+fi
+ac_json=$(curl https://raw.githubusercontent.com/StakedChain/StakedNotary/master/assetchains.json 2>/dev/null)
+for row in $(echo "${ac_json}" | jq -c -r '.[]'); do
+	_jq() {
+		echo ${row} | jq -r ${1}
+	}
+	chain=$(_jq '.ac_name')
+	if [ ! -d ~/.komodo/${chain}  ]; then
+		echo -e "\e[91m [ $chain ] CONF FILE DOES NOT EXIST!"
+                echo -e "Run ~/Knomp/install/startStaked.sh first \e[39m"
+		exit 1
+	fi
+	if [ ! -f  ~/kmd_pulp/stomp/wallets/.${chain}_wallet ]; then
+		touch  ~/kmd_pulp/stomp/wallets/.${chain}_wallet
+		sudo chmod 600  ~/kmd_pulp/stomp/wallets/.${chain}_wallet
+		address=$(komodo-cli -ac_name=$chain getnewaddress)
+		echo "Created $address for [ $chain ]"
+		echo { \"chain\":\"${chain}\", >> ~/kmd_pulp/stomp/wallets/.${chain}_wallet
+		echo \"addr\":\"${address}\", >> ~/kmd_pulp/stomp/wallets/.${chain}_wallet
+		echo \"pk\":\"$(komodo-cli -ac_name=${chain} dumpprivkey $address)\", >> ~/kmd_pulp/stomp/wallets/.${chain}_wallet
+		echo \"pub\":\"$(komodo-cli -ac_name=${chain} validateaddress $address | jq -r '.pubkey')\" } >> ~/kmd_pulp/stomp/wallets/.${chain}_wallet
+	else
+		echo "ADDRESS FOR $chain ALREADY CREATED";
+	fi
+done
+echo -e "\e[92m Finished: Your address info is located in ~/kmd_pulp/stomp/wallets \e[39m"
