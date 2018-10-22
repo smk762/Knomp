2468adea43fc4a3bb0a31b00af687bcbad1f664c
smk762 <smk762@iinet.net.au>
2018-10-22 10:30:28 +0000

    fresh

--- genaddr.sh
+++ genaddr.sh
@@ -1,34 +1,47 @@
 #!/bin/bash
 
+checkSync () {
+	ac_name=$1
+}
+
 #Get Asset Chain Names from json file
 echo -e "\e[91m WARNING: This script creates addresses to be use in pool config and payment processing"
 echo " The address, privkey, and pubkey are stored in a owner read-only file"
 echo -e " make sure to encrypt, backup, or delete as required \e[39m"
-if [ ! -d ~/kmd_pulp/stomp/wallets  ]; then
-	mkdir ~/kmd_pulp/stomp/wallets
+if [ ! -d ~/wallets  ]; then
+	mkdir ~/wallets
 fi
 ac_json=$(curl https://raw.githubusercontent.com/StakedChain/StakedNotary/master/assetchains.json 2>/dev/null)
 for row in $(echo "${ac_json}" | jq -c -r '.[]'); do
 	_jq() {
 		echo ${row} | jq -r ${1}
 	}
-	chain=$(_jq '.ac_name')
-	if [ ! -d ~/.komodo/${chain}  ]; then
-		echo -e "\e[91m [ $chain ] CONF FILE DOES NOT EXIST!"
-                echo -e "Run ~/Knomp/install/startStaked.sh first \e[39m"
+	ac_name=$(_jq '.ac_name')
+	if [ ! -d ~/.komodo/${ac_name}  ]; then
+		echo -e "\e[91m [ $ac_name ] CONF FILE DOES NOT EXIST!"
+                echo -e "Run ~/Knomp/startStaked.sh first \e[39m"
 		exit 1
 	fi
-	if [ ! -f  ~/kmd_pulp/stomp/wallets/.${chain}_wallet ]; then
-		touch  ~/kmd_pulp/stomp/wallets/.${chain}_wallet
-		sudo chmod 600  ~/kmd_pulp/stomp/wallets/.${chain}_wallet
-		address=$(komodo-cli -ac_name=$chain getnewaddress)
-		echo "Created $address for [ $chain ]"
-		echo { \"chain\":\"${chain}\", >> ~/kmd_pulp/stomp/wallets/.${chain}_wallet
-		echo \"addr\":\"${address}\", >> ~/kmd_pulp/stomp/wallets/.${chain}_wallet
-		echo \"pk\":\"$(komodo-cli -ac_name=${chain} dumpprivkey $address)\", >> ~/kmd_pulp/stomp/wallets/.${chain}_wallet
-		echo \"pub\":\"$(komodo-cli -ac_name=${chain} validateaddress $address | jq -r '.pubkey')\" } >> ~/kmd_pulp/stomp/wallets/.${chain}_wallet
+	if [ ! -f  ~/wallets/.${ac_name}_poolwallet ]; then
+		komodod -ac_name=$ac_name > /dev/null 2>&1 &
+		sleep 15
+		echo "getting address for $ac_name"
+		address=$(komodo-cli -ac_name=$ac_name getnewaddress) > /dev/null 2>&1
+		while [[ ${#address} -ne 34 ]]; do
+			echo "trying to get address for $ac_name again in 10 seconds"
+			address=$(komodo-cli -ac_name=$ac_name getnewaddress) > /dev/null 2>&1
+			sleep 10
+		done
+		echo "New address $address created for $ac_name"
+		touch  ~/wallets/.${ac_name}_poolwallet
+		chmod 600  ~/wallets/.${ac_name}_poolwallet
+		echo { \"ac_name\":\"${ac_name}\", > ~/wallets/.${ac_name}_poolwallet
+		echo \"ac_addr\":\"${address}\", >> ~/wallets/.${ac_name}_poolwallet
+		echo \"ac_pk\":\"$(komodo-cli -ac_name=${ac_name} dumpprivkey $address)\", >> ~/wallets/.${ac_name}_poolwallet
+		echo \"ac_pub\":\"$(komodo-cli -ac_name=${ac_name} validateaddress $address | jq -r '.pubkey')\" } >> ~/wallets/.${ac_name}_poolwallet
 	else
-		echo "ADDRESS FOR $chain ALREADY CREATED";
+		echo "ADDRESS FOR $ac_name ALREADY CREATED";
+		cat  ~/wallets/.${ac_name}_poolwallet;
 	fi
 done
-echo -e "\e[92m Finished: Your address info is located in ~/kmd_pulp/stomp/wallets \e[39m"
+echo -e "\e[92m Finished: Your address info is located in ~/wallets/.${ac_name}_poolwallet \e[39m"
